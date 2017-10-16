e4214fa803d98b6653dcf5cc333942605c221047
Nikolas Mpegetis <n.mpegetis@silensec.com>
2017-10-16 17:58:51 +0300

    feat(client/../dashboard): Center and zoom to cover all visible markers.
    Closes #3

--- client/views/dashboard/dashboard.js
+++ client/views/dashboard/dashboard.js
@@ -12,35 +12,36 @@ var bounds;
 
 Template.map.helpers({
     mapOptions: function () {
-        // console.log(Markers.find({_id: Meteor.user()._id}).fetch());
-        console.log(Markers.find({}).fetch());
         var markers = Markers.find({}).fetch();
         bounds = new google.maps.LatLngBounds();
         for (var i = 0; i < markers.length; i++) {
-            console.log(bounds)
-            console.log(markers[i]);
-            // bounds.extend(markers[i].getPosition());
-            bounds.extend({lat:markers[i].lat, lng:markers[i].lng});
-            // bounds.extend(new google.maps.LatLng(markers[i].lat, markers[i].lng));
+            // console.log(markers[i]);
+            bounds.extend({lat: markers[i].lat, lng: markers[i].lng});
         }
 
-        console.log(bounds)
-        // setTimeout( function() { map.instance.fitBounds( bounds ); }, 1 );
-//            map.instance.fitBounds(bounds)
-GoogleMaps.ready('map', function (map) {
-    if (Meteor.user().roles == 'admin') {
-        map.instance.fitBounds(bounds)
-    }
-    else {
-//        map.instance.fitBounds(bounds);
-        map.instance.setZoom(14)
-    }
-});
+
+        GoogleMaps.ready('map', function (map) {
+            //center the map to the geometric center of all markers
+            map.instance.setCenter(bounds.getCenter());
+
+            map.instance.fitBounds(bounds)
+
+            if (Meteor.user().roles == 'admin') {
+                //remove one zoom level to ensure no marker is on the edge.
+                map.instance.setZoom(map.instance.getZoom()-1);
+
+                // set a minimum zoom
+                // if you got only 1 marker or all markers are on the same address map will be zoomed too much.
+                if(map.instance.getZoom()> 14){
+                    map.instance.setZoom(14);
+                }
+            }
+            else {
+                map.instance.setZoom(15)
+            }
+        });
         if (GoogleMaps.loaded()) {
             return {
-                // todo na bazw na ginetai zoom wste na kaluptei oles tis poleis pou exei pinezes gia ton admin. Gia ton user na ginetai stin poli pou einai
-                // center: new google.maps.LatLng(markers[0].lat, markers[0].lng), //Athens
-//todo na balw edw to lat lng tis thesis
                 center: new google.maps.LatLng(38.0213, 23.7986), //Athens
                 zoom: 10
             };
@@ -469,8 +470,6 @@ Template.map.onCreated(function () {
             });
 
 
-
-
             // console.log(Markers.find({_id: Meteor.user()._id}).fetch());
             console.log(Markers.find({}).fetch());
             var markers = Markers.find({}).fetch();
@@ -483,19 +482,9 @@ Template.map.onCreated(function () {
             }
 
 
-
-
-
-
-
-
-
-
-
-
 // console.log(bounds)
 //                 setTimeout( function() { map.instance.fitBounds( bounds ); }, 1000 );
-           // map.instance.fitBounds(bounds);
+            // map.instance.fitBounds(bounds);
 
         }
 
